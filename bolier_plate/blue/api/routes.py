841b55c4348f98bcfa6e3a681369256e278033e0
humnaS <humna.shafqatiqbal@gmail.com>
2020-07-02 15:33:05 +0500

    adding path

--- bolier_plate/blue/api/routes.py
+++ bolier_plate/blue/api/routes.py
@@ -3,6 +3,21 @@ from flask_restful import Api,Resource
 import pandas as pd
 from werkzeug.utils import secure_filename
 
+
+import sys 
+import os
+import json
+
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+dirname_list = dirname.split("\\")[:-1]
+dirname = "\\".join(dirname_list)
+print(dirname)
+path = dirname + "\\api"
+print(path)
+sys.path.append(path)
+
+
 mod = Blueprint('api',__name__)
 api = Api(mod)
 df = None
